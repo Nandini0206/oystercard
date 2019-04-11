4632df320c5b29f9d4e43c23b61d3eee9f042c57
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-04-11 15:44:07 +0100

    13.created test file and lib file for Station class exposing name and zone variable

--- lib/station.rb
+++ lib/station.rb
@@ -1,5 +1,8 @@
-Class Station
+class Station
+attr_reader :name, :zone
 
-
-
+  def initialize(name, zone)
+    @name = name
+    @zone = zone
+  end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,4 +2,14 @@ require 'station'
 
 describe Station do
 
+  describe '#initialize' do
+    subject = Station.new(:name, :zone)
+    it 'takes the station name' do
+      expect(subject.name).to eq :name
+    end
+
+    it 'take the zone' do
+      expect(subject.zone).to eq :zone
+    end
+  end
 end
